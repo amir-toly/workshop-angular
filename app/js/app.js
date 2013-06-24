7994a1607849d57a74fa8728ff708f5704ba83c3
Amir TOLY <amir.toly@gmail.com>
2013-06-24 18:54:28 +0200

    Filter logs by HTTP statuses & methods

--- app/js/app.js
+++ app/js/app.js
@@ -67,4 +67,36 @@ function LogCtrl($scope) {
 			"date": "01/01/2013 01:05:00"
 		}
 	];
+	$scope.selectedStatus = {
+			"200": true,
+			"404": true,
+			"500": true
+	};
+	$scope.selectedMethods = {
+			"GET": true,
+			"POST": true,
+			"PUT": true,
+			"DELETE": true
+	};
+	$scope.selectedLogs = [];
+	var filterLogs = function() {
+		$scope.selectedLogs = [];
+		
+		for (var i = 0; i < $scope.logs.length; i++) {
+			var statusPresent = false;//TODO(use a set to avoid duplicates)
+			for (var status in $scope.selectedStatus) {
+				if ($scope.logs[i].status == status && $scope.selectedStatus[status]) {
+					statusPresent = true;
+				}
+			}//TODO(is there a contains() function in JS?)
+			
+			for (var method in $scope.selectedMethods) {
+				if ($scope.logs[i].method == method && $scope.selectedMethods[method] && statusPresent) {
+					$scope.selectedLogs[$scope.selectedLogs.length] = $scope.logs[i];
+				}
+			}//TODO(is there a contains() function in JS?)
+		}//TODO(use loop iterators instead of indexes)
+	};
+	$scope.$watchCollection('selectedStatus', filterLogs);
+	$scope.$watchCollection('selectedMethods', filterLogs);
 }
